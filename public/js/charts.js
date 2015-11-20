281af9d6834ea0a273546e11bc7061de5dd61450
Goran Mikulic <mikulicg@gmail.com>
2015-11-20 12:00:50 +0100

    cleaned up frontend code

--- public/js/charts.js
+++ public/js/charts.js
@@ -37,5 +37,5 @@ var jsonResponse = JSON.parse(this.responseText);
         });
 };
 
-oReq.open("get", "http://nodejs-mikugo.rhcloud.com/wifidevicescount?fy=2015&fm=11&fd=10&ty=2015&tm=11&td=20", true);
+oReq.open("get", "/wifidevicescount?fy=2015&fm=11&fd=10&ty=2015&tm=11&td=20", true);
 oReq.send();
