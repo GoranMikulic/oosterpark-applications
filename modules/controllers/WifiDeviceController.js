b4828ee658eb5438fcee47ace2e0ed1b2d4bdfe8
Goran Mikulic <mikulicg@gmail.com>
2015-11-22 21:26:20 +0100

    refactoring - abstracted methods

--- modules/controllers/WifiDeviceController.js
+++ modules/controllers/WifiDeviceController.js
@@ -1,5 +1,5 @@
 var WifiDevice = require('../models/WifiDevice');
-var MyUtils = require('../utils/Utils');
+var Utils = require('../utils/Utils');
 
 module.exports = {
   /**
@@ -7,7 +7,7 @@ module.exports = {
    */
   getWifiDeviceById: function(req, res) {
     WifiDevice.getWifiDeviceById(req.params.device_id, function(rows) {
-      MyUtils.returnResult(res, rows);
+      Utils.returnResult(res, rows);
     });
   },
 
@@ -19,15 +19,15 @@ module.exports = {
     var startDate = req.query.startdate;
     var endDate = req.query.enddate;
 
-    var isValidPeriodQuery = MyUtils.checkPeriodQuery(startDate, endDate);
+    var isValidPeriodQuery = Utils.checkPeriodQuery(startDate, endDate);
 
     if (!isValidPeriodQuery) {
       WifiDevice.queryAllDevices(function(rows) {
-        MyUtils.returnResult(res, rows)
+        Utils.returnResult(res, rows)
       });
     } else {
       WifiDevice.queryDeivcesInPeriod(startDate, endDate, function(rows) {
-        MyUtils.returnResult(res, rows);
+        Utils.returnResult(res, rows);
       });
     }
 
@@ -39,18 +39,18 @@ module.exports = {
     var startDate = req.query.startdate;
     var endDate = req.query.enddate;
 
-    var isValidPeriodQuery = MyUtils.checkPeriodQuery(startDate, endDate);
+    var isValidPeriodQuery = Utils.checkPeriodQuery(startDate, endDate);
 
     if (isValidPeriodQuery) {
       WifiDevice.queryDeivcesInPeriod(startDate, endDate, function(queryResult) {
 
-        var datesBetween = MyUtils.getDatesBetween(new Date(startDate), new Date(endDate));
-        var result = getDevicesCountsForDays(datesBetween, queryResult, 'first_time_seen', getDevicesCountForDay);
+        var datesBetween = Utils.getDatesBetween(new Date(startDate), new Date(endDate));
+        var result = getDevicesCountsForTimeRange(datesBetween, queryResult, 'first_time_seen', isDayEqual, 'Amount of Wifi-Devices');
 
-        MyUtils.returnResult(res, result);
+        Utils.returnResult(res, result);
       });
     } else {
-      MyUtils.returnResult(res, {});
+      Utils.returnResult(res, {});
     }
 
   },
@@ -62,40 +62,35 @@ module.exports = {
 
     if (typeof day !== 'undefined') {
       var start = day + ' 00:01:00';
-      var end = day + ' 23:00:00';
+      var end = day + ' 23:59:00';
 
       WifiDevice.queryDeivcesInPeriod(start, end, function(queryResult) {
+        var hours = getClockHours(day);
+        var result = getDevicesCountsForTimeRange(hours, queryResult, 'first_time_seen', isHourEqual, 'Amount of Wifi-Devices');
 
-        var result = getDevicesCountForHours(day, queryResult, 'first_time_seen');
-
-        MyUtils.returnResult(res, result);
+        Utils.returnResult(res, result);
       });
     } else {
-      MyUtils.returnResult(res, {});
+      Utils.returnResult(res, {});
     }
-
   },
 }
 
 /**
  * Returns the amounts of devices for a period of days
  */
-var getDevicesCountsForDays = function(datesBetween, devicesArray, datePropertyName, countCalculatorFunction) {
+var getDevicesCountsForTimeRange = function(timeRange, devicesArray, datePropertyName, comparator, dataLabel) {
   //array for date values
   var dates = new Array();
   dates.push('x');
   //array for amount of devices
   var counts = new Array();
-  counts.push('Amount of Wifi-Devices');
+  counts.push(dataLabel);
 
-  for (var date in datesBetween) {
-
-    var day = datesBetween[date];
-    var counter = countCalculatorFunction(day, devicesArray, datePropertyName);
-
-    dates.push(datesBetween[date]);
+  for (var time in timeRange) {
+    var counter = getDevicesCount(timeRange[time], devicesArray, datePropertyName, comparator);
+    dates.push(timeRange[time]);
     counts.push(counter);
-
   }
 
   return {
@@ -104,56 +99,58 @@ var getDevicesCountsForDays = function(datesBetween, devicesArray, datePropertyN
   };
 }
 
-var getDevicesCountForHours = function(day, devicesArray, datePropertyName) {
-  var hours = new Array();
-  var hoursResult = new Array();
-  hoursResult.push('x');
+/**
+ * Returns amount of devices for a particular date,
+ * values are compared with the comparator function
+ */
+var getDevicesCount = function(time, devicesArray, datePropertyName, comparator) {
+  var counter = 0;
 
-  var counts = new Array();
-  counts.push('Amount of Wifi-Devices');
-
-  for (var i = 0; i < 24; i++) {
-    hours.push(i);
-  }
-
-  for (var hour in hours) {
-    var counter = 0;
-
-    for (var device in devicesArray) {
-      var timeCaptured = devicesArray[device][datePropertyName].getHours();
-
-      if (timeCaptured == hour) {
-        counter++;
-      }
+  for (var device in devicesArray) {
+    var compareResult = comparator(devicesArray[device][datePropertyName], time);
+    if (compareResult) {
+      counter++;
     }
-
-    hoursResult.push(day + ' ' + hour + ':00:00');
-    counts.push(counter);
   }
-
-  return {
-    x: hoursResult,
-    counts: counts
-  };
-
+  return counter;
 }
 
 /**
- * Returns the amount of devices for a day
+ * Returns true if the hour matches
  */
-var getDevicesCountForDay = function(day, devicesArray, datePropertyName) {
-  var counter = 0;
-
-  for (var row in devicesArray) {
-    var dayCaptured = devicesArray[row][datePropertyName];
-
-    //Set time to 00:00:00 to compare days only
-    dayCaptured.setHours(0, 0, 0, 0);
-    day.setHours(0, 0, 0, 0);
-    if (dayCaptured.getTime() == day.getTime()) {
-      counter++;
-    }
-
+var isHourEqual = function(deviceCaptureTime, timeToCompare) {
+  if (deviceCaptureTime.getHours() == timeToCompare.getHours()) {
+    return true;
   }
-  return counter;
+  return false;
+}
+
+/**
+ * Compares true if the day matches
+ */
+var isDayEqual = function(deviceCaptureTime, timeToCompare) {
+  //Set time to 00:00:00 to compare days only
+  deviceCaptureTime.setHours(0, 0, 0, 0);
+  timeToCompare.setHours(0, 0, 0, 0);
+
+  if (deviceCaptureTime.getTime() == timeToCompare.getTime()) {
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * Returns an array of date objects of
+ * the given day with hours set from 0 to 23
+ */
+var getClockHours = function(day) {
+  var hours = new Array();
+  for (var i = 0; i < 24; i++) {
+    var date = new Date(day);
+    date.setHours(i);
+    date.setMinutes(0, 0, 0);
+    hours.push(date);
+  }
+  return hours;
 }
