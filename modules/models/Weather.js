6dde3f998b80de32a4ad269f0a457dbf77549821
Goran Mikulic <mikulicg@gmail.com>
2015-12-16 21:45:43 +0100

    refactoring

--- modules/models/Weather.js
+++ modules/models/Weather.js
@@ -4,11 +4,12 @@ var connection = require('../utils/MySql').connection();
 
 module.exports = {
   saveWeatherInfo: function(weatherData) {
-    console.log("WD " + weatherData);
-    var query = connection.query('INSERT INTO weatherchart SET ?', weatherData, function(err, result){
-      console.log("Inserted " + err);
-    });
 
-    console.log(query.sql);
+    var query = connection.query('INSERT INTO weatherchart SET ?', weatherData, function(err, result){
+      if(!err) {
+          console.log("Inserted weather data for " + weatherData.date);
+      }
+    });
+    
   }
 }
