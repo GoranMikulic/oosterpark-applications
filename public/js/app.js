03f52b6ae695b45c8b6cf26d049491821f1a6656
Goran Mikulic <mikulicg@gmail.com>
2015-12-23 16:22:19 +0100

    organized widgets in views

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,22 @@
 (function() {
 
-  angular.module('dataAnalizingApp', ['btford.socket-io']);
+  angular.module('dataAnalizingApp', ['btford.socket-io', 'ngRoute'])
+
+  .config(function($routeProvider) {
+
+    $routeProvider
+      .when('/', {
+        templateUrl: '/js/views/analyze.html'
+      })
+      .when('/compare', {
+        templateUrl: '/js/views/analyze.html'
+      })
+      .when('/livedata', {
+        templateUrl: '/js/views/realtimewifi.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
 
 })();
